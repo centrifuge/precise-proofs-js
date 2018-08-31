b8776c03e4814784aaf434cfe088cd552116244a
Razvan Dinicut <razvan.dinicut@gmail.com>
2018-08-31 10:20:29 +0200

    add hex and ordered hashes support  (#4)
    * validate proofs with sorted hashes
    
    * update readme
    
    * add sortedHash logic and cleanup readme

--- README.md
+++ README.md
@@ -4,29 +4,62 @@ Precise Proofs Js
 Read the [introduction on Precise-Proofs](https://github.com/centrifuge/precise-proofs)
 
 Precise Proofs Js is a library for validating fields/leafs from  Merkle proofs created with [precise-proofs](https://github.com/centrifuge/precise-proofs/blob/master/README.md). 
-The library support 2   hashing functions:  SHA2_256 and SHA3_256 and it uses SHA2_256 out of the box.
+The library supports 2   hashing functions(SHA2_256SHA3_256) and hex or base base64 encoding for the hashes. Out of the box it uses hex encoding and SHA2_256.
 
-In case you generate your proofs with SHA3_256 you can swich using the following code:
+In case you generate your proofs with SHA3_256 and have them base64 encoded. You can switch using the following code:
 
 ```javascript
 import {PreciseProofs, SHA3_256} from "precise-proofs-js";
 
-const preciseProofs = new PreciseProofs(SHA3_256);
+const preciseProofs = new PreciseProofs('base64',SHA3_256);
 ```
+
+
 
 ## Supported Proof format
 
 ```js,
+  
+// Sorted Hashes
+{
+    "property": "valueA",
+    "value": "Example",
+    "salt": "d555901541825e5d40612d220142c7428c385c48e0245fd3a40b8e3b64679be1",
+    "sortedHashes": [
+        "60c3dfcdd85fcef1d4826d4695fb5064aa8434d014f2c1547f9398bdf72bdb75",
+        "8951fa82c3f5563d88769a9ecb8f6e03b389c06ff64a1b26dc5898cefe95a42f",
+        "46d0a998333fc2c8b4cfff1d5e122f7c32ff46f0a4410f63821f45cc02fd3723"
+    ]
+}
+
+
+// Hex with our without the 0x prefix
+{
+    "property": "valueA",
+    "value": "Example",
+    "salt": "d555901541825e5d40612d220142c7428c385c48e0245fd3a40b8e3b64679be1",
+    "hashes":[  
+        { "right": "60c3dfcdd85fcef1d4826d4695fb5064aa8434d014f2c1547f9398bdf72bdb75=" },
+        { "left": "8951fa82c3f5563d88769a9ecb8f6e03b389c06ff64a1b26dc5898cefe95a42f" },
+        { "right": "46d0a998333fc2c8b4cfff1d5e122f7c32ff46f0a4410f63821f45cc02fd3723" }
+    ]
+}
+    
+    
+// Base64 Ecoded
 {  
     "property":"ValueA",
     "value":"Example",
     "salt":"1VWQFUGCXl1AYS0iAULHQow4XEjgJF/TpAuOO2Rnm+E=",
     "hashes":[  
-        { "right":"kYXAGhDdPiFMq1ZQMOZiKmSf1S1eHNgJ6BIPSIExOj8=" },
-        { "left":"GDgT7Km6NK6k4N/Id4CZXErL3p6clNX7sVnlNyegdG0=" },
-        { "right":"qOZzS+YM8t1OfC87zEKgkKz6q0f3wwk5+ed+PR/2cDA=" }
+        { "right": "kYXAGhDdPiFMq1ZQMOZiKmSf1S1eHNgJ6BIPSIExOj8=" },
+        { "left":" GDgT7Km6NK6k4N/Id4CZXErL3p6clNX7sVnlNyegdG0=" },
+        { "right": "qOZzS+YM8t1OfC87zEKgkKz6q0f3wwk5+ed+PR/2cDA=" }
     ]
 }
+
+
+    
 ```
 
 
@@ -37,8 +70,27 @@ const preciseProofs = new PreciseProofs(SHA3_256);
 ```javascript,
 import {PreciseProofs} from "precise-proofs-js";
 
-const rootHash = "Qpvx6jMpfee4eXYeDm+DO+Z8iArdnYdm3J3BH0AUxHU=";
-const proof = {
+const rootHash = "29dbffa68e5cd48bb4cb2504190f10883fb187793b2f70eab81fb5440c6809b6";
+let proof = {
+    "property": "valueA",
+    "value": "Example",
+    "salt": "d555901541825e5d40612d220142c7428c385c48e0245fd3a40b8e3b64679be1",
+    "hashes": [
+        "60c3dfcdd85fcef1d4826d4695fb5064aa8434d014f2c1547f9398bdf72bdb75",
+        "8951fa82c3f5563d88769a9ecb8f6e03b389c06ff64a1b26dc5898cefe95a42f",
+        "46d0a998333fc2c8b4cfff1d5e122f7c32ff46f0a4410f63821f45cc02fd3723"
+    
+    ]
+}
+
+const preciseProofs = new PreciseProofs();
+const valid = preciseProofs.isValidField(proof, rootHash)
+// valid  === true
+
+
+// With base64 encoding
+const base64RootHash = "Qpvx6jMpfee4eXYeDm+DO+Z8iArdnYdm3J3BH0AUxHU=";
+const base64Proof = {
     "property": "value1",
     "value": "1",
     "salt": "Bs89XQyxE05c57R5KVb8aFk7rqf+A5XUgxlH3t1hRoY=",
@@ -55,14 +107,12 @@ const proof = {
     ]
 }
 
-const preciseProofs = new PreciseProofs();
+const base64PreciseProofs = new PreciseProofs('base64');
     
-const valid = preciseProofs.isValidField(proof, rootHash)
-// valid  === true 
+const base64Valid = base64PreciseProofs.isValidField(base64Proof, base64RootHash)
+// base64Valid  === true 
 ```
 
 ### Missing features
 The following features are being worked on:
-* support Hex encoded hashes
 * construct tree and generate proof
-* Support for nested documents
--- src/index.js
+++ src/index.js
@@ -5,57 +5,85 @@ export const SHA2_256 = "SHA2_256";
 export const SHA3_256 = "SHA3_256";
 
 const hashFunction = (value, hashType) => {
-  switch (hashType) {
-    case SHA2_256: {
-      return Buffer.from(sha2_256.array(value));
+    switch (hashType) {
+        case SHA2_256: {
+            return Buffer.from(sha2_256.array(value));
+        }
+        case SHA3_256: {
+            return Buffer.from(sha3_256.array(value));
+        }
+        default:
+            throw new Error(`Hash ${hashType} type not supported`);
     }
-    case SHA3_256: {
-      return Buffer.from(sha3_256.array(value));
-    }
-    default:
-      throw new Error(`Encryption ${hashType} type not supported`);
-  }
 };
 
+const toBuffer = (value, type) => {
+    if (type === 'hex' && value.match(/^0x/)) {
+        value = value.slice(2);
+    }
+    return Buffer.from(value, type);
+}
+
 const hashTypeSymbol = Symbol("hashType");
+const hashFunctionSymbol = Symbol("hashFunction");
 
 export class PreciseProofs {
 
-  constructor(hashType = SHA2_256) {
-    this[hashTypeSymbol] = hashType;
-  }
+    constructor(hashType = "hex", hashFunction = SHA2_256) {
+        this[hashFunctionSymbol] = hashFunction;
+        this[hashTypeSymbol] = hashType;
+    }
 
-  // Checks the format of a field proof
-  static isValidFieldFormat(proof) {
-    return proof && proof.property && proof.value && proof.salt && proof.hashes && Array.isArray(proof.hashes);
-  };
+    // Checks the format of a field proof
+    static isValidFieldFormat(proof) {
+        return (
+            proof &&
+            proof.property &&
+            proof.value &&
+            proof.salt &&
+            (proof.hashes || proof.sortedHashes) &&
+            (Array.isArray(proof.hashes) || Array.isArray(proof.sortedHashes))
+        )
+    };
+
+    // Validates a field proof returns true or false
+    isValidField(proof, rootHash) {
+
+        if (!PreciseProofs.isValidFieldFormat(proof)) throw new Error('Field proof format is invalid, please read the documentation');
+        if (proof.hashes && proof.sortedHashes) throw new Error('Proof can not have hashes and sortedHashes prop at the same time');
+
+        let root = toBuffer(rootHash, this[hashTypeSymbol]);
+        // Create hash from cocatenated property name, value and salt
+        let proofHash = hashFunction(Buffer.concat([toBuffer(proof.property, 'ascii'), toBuffer(proof.value, 'ascii'), toBuffer(proof.salt, this[hashTypeSymbol])]), this[hashFunctionSymbol]);
+        let resultHash;
+
+        if(proof.hashes) {
+            // Handle one property/leaf documents
+            if (proof.hashes.length === 0) return root.toString('hex') === proofHash.toString('hex');
+            resultHash = proof.hashes.reduce((acc, currentValue) => {
+                if (currentValue.left) {
+                    return hashFunction(Buffer.concat([toBuffer(currentValue.left, this[hashTypeSymbol]), acc]), this[hashFunctionSymbol]);
+                } else if (currentValue.right) {
+                    return hashFunction(Buffer.concat([acc, toBuffer(currentValue.right, this[hashTypeSymbol])]), this[hashFunctionSymbol]);
+                } else {
+                    throw new Error(`Bad format for hash object: ${currentValue}. It should have a left or a right property`)
+                }
+            }, proofHash);
+        } else if(proof.sortedHashes) {
+            // Handle one property/leaf documents
+            if (proof.sortedHashes.length === 0) return root.toString('hex') === proofHash.toString('hex');
+            resultHash = proof.sortedHashes.reduce((acc, currentValue) => {
+                if (typeof currentValue === 'string') {
+                    return hashFunction(Buffer.concat([acc, toBuffer(currentValue, this[hashTypeSymbol])].sort(Buffer.compare)), this[hashFunctionSymbol]);
+                }  else {
+                    throw new Error(`Bad format for hash object: ${currentValue}. It should be a string hash`)
+                }
+            }, proofHash);
+        }
 
 
-  // Validates a field proof returns true or false
-  isValidField(proof, rootHash) {
-
-    if (!PreciseProofs.isValidFieldFormat(proof)) throw new Error('Field proof format is invalid, please read the documentation');
-
-    let root = Buffer.from(rootHash, 'base64');
-
-    // Create hash from cocatenated property name, value and salt
-    let proofHash = hashFunction(Buffer.concat([Buffer.from(proof.property), Buffer.from(proof.value), Buffer.from(proof.salt, 'base64')]), this[hashTypeSymbol]);
-
-    // Handle one property/leaf documents
-    if (proof.hashes.length === 0) return root.toString('hex') === proofHash.toString('hex');
-
-    let resultHash = proof.hashes.reduce((acc, currentValue) => {
-      if (currentValue.left) {
-        return hashFunction(Buffer.concat([Buffer.from(currentValue.left, 'base64'), acc]), this[hashTypeSymbol]);
-      } else if (currentValue.right) {
-        return hashFunction(Buffer.concat([acc, Buffer.from(currentValue.right, 'base64')]), this[hashTypeSymbol]);
-      } else {
-        throw new Error(`Bad format for hash object: ${currentValue}. It should have a left or a right property`)
-      }
-    }, proofHash);
-
-    return resultHash.toString('hex') === root.toString('hex');
-  };
+        return resultHash.toString('hex') === root.toString('hex');
+    };
 
 }
 
